ee0c3195732167ce25bddbf1ea1a58d6c9fd1b24
magestus <magestus@yandex.ru>
2016-06-10 00:23:35 +0500

    1

---
+++ PythonStarter_lesson3/condition.py
@@ -0,0 +1,3 @@
+is_ready = ['qwe']
+is_ready.append('11')
+print(is_ready)
---
+++ PythonStarter_lesson3/if_else.py
@@ -0,0 +1,6 @@
+name = input('Enter your name: ')
+if name == 'Max':
+        print('You have entered', name)
+        print('This my name, too.')
+else:
+        print('Your name differs from mine.')
