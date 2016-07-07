15666b8e4b4711fee43f1afe1d04b0bd06bb0873
magestus <magestus@yandex.ru>
2016-07-07 09:58:50 +0500

    1

---
+++ Learning_Mark Sammerfield/Modules.py
@@ -0,0 +1,21 @@
+import string
+
+def clear(text, whitespace=string.whitespace, delete=''):
+   result = []
+   word = ""
+   for char in text:
+       if char in delete:
+            continue
+       elif char in whitespace:
+            if word:
+                result.append(word)
+                word = ""
+            else:
+                word += char
+       if word:
+                result.append(word)
+       
+       return " ".join(result)
+ 
+
+   print(clear('nichosi', delete = 'i'))
