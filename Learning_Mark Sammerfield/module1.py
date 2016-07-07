1c8c2df9d11390bc583990cd6994defc447d74e6
magestus <magestus@yandex.ru>
2016-07-07 14:58:19 +0500

    1

---
+++ Learning_Mark Sammerfield/module1.py
@@ -0,0 +1,13 @@
+import chargrid
+resize(14, 50)
+chargrid.add_rectangle(0, 0, *get_size())
+add_vertical_line(5, 10, 13)
+add_vertical_line(2, 9, 12, "!")
+add_horizontal_line(3, 10, 20, "+")
+add_rectangle(0, 0, 5, 5, "%")
+add_rectangle(5, 7, 12, 40, "#", True)
+add_rectangle(7, 9, 10, 38, " ")
+add_text(8, 10, "This is the CharGrid module")
+add_text(1, 32, "Pleasantville", "@")
+add_rectangle(6, 42, 11, 46, fill=True)
+render(False)
